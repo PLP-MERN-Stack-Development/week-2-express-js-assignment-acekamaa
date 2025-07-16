406df6cb789cc546c7de37c775f93a4792f5632a
acekamaa <kvnkamaa@gmail.com>
2025-07-17 00:19:23 +0300

    add routes

---
+++ routes/productRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/productController');
+const auth = require('../middleware/auth');
+const validate = require('../middleware/validateProduct');
+
+router.get('/', controller.getAll);
+router.get('/:id', controller.getById);
+router.post('/', auth, validate, controller.create);
+router.put('/:id', auth, validate, controller.update);
+router.delete('/:id', auth, controller.remove);
+
+module.exports = router;
